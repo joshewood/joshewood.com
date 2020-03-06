bf7f4609c32e2e5d002f94a252de0e74d1c2e14f
Josh Wood <josh@coastalcannabiscollective.com>
2020-03-05 20:53:12 -0800

    🎨 final tweaks to the slider, give the intro panel OOMF

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,32 +2,42 @@ import React, { useState, useEffect  } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-const adjectives = ['thoughtful', 'caring', 'passionate', 'humble', 'perceptive', 'friendly', 'professional', 'resourceful', 'front-end', 'back-end', 'senior'];
+const adjectives = ['thoughtful', 'creative', 'caring', 'passionate', 'humble', 'perceptive', 'friendly', 'professional', 'resourceful', 'front-end', 'back-end', 'senior'];
 const consumeAdjective = () => adjectives.sort(() => { return 0.5 - Math.random();}).pop();
 const isAnimationComplete = () => (adjectives.length === 0);
 function App() {
   
   const [currentAdjective, setAdjective] = useState(consumeAdjective());
+  const [fastForward, setFastForward] =  useState(false);
   useEffect(() => {
-    
-    const interval = setInterval(() => {      
+    let iteration = 0;
+    let interval;
+    const tick = () => {
+      iteration++;
       const randomAdjective = consumeAdjective();
       setAdjective(randomAdjective);
       if (isAnimationComplete()) {
         clearInterval(interval);
         return;
       }
-    }, 1000)
+    }
 
-    
+    interval = setInterval(() => {
+      if (iteration == 1) {
+        clearInterval(interval);
+        interval = setInterval(tick, 250);
+        setFastForward(true);
+      }
+      tick();
+    }, 1000)
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="IntroPanel">
         <h1>Hey, I'm <span className="Accent">Josh</span>.</h1>
         <h2 className="AlignChildrenVertical">
           <span>I'm a </span>
-          <span className={`WordCycler${(isAnimationComplete()) ? ' Settle' : ''}`}>
+          <span className={`WordCycler${(isAnimationComplete()) ? ' Settle' : ''}${(fastForward) ? ' FastForward' : ''}`}>
             <span className="WordCyclePlane">{currentAdjective}</span>
           </span> 
           <span>developer.</span>
