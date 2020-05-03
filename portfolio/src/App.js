c4283be582d18b3082f9cc2598bc8068d19f7b11
Josh Wood <gdjoshwood@gmail.com>
2020-05-03 14:56:58 -0700

    🚀 major layout changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect, useMemo  } from 'react';
 import logo from './logo.svg';
 import { SocialIcon } from 'react-social-icons';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { AnimatedSwitch } from 'react-router-transition';
 import './App.scss';
 
 const adjectives = [
@@ -19,6 +25,7 @@ function App() {
   const [currentAdjective, setAdjective] = useState(consumeAdjective());
   const [fastForward, setFastForward] =  useState(false);
   const [CTAEffect, changeCTAEffect] = useState('Empty');
+  const [hasLoaded, setHasLoaded] = useState(false);
   let downloadResume = () => {
     changeCTAEffect('Engaged');
     setTimeout(()=> {
@@ -31,6 +38,7 @@ function App() {
     window.location.assign('resume.pdf');
   };
   useEffect(() => {
+    setHasLoaded(true);
     beginEntrance(true);
     let iteration = 0;
     let interval;
@@ -54,63 +62,77 @@ function App() {
     }, 1000)
   }, []);
   return (
-    <div className="App">
-      <div className={`IntroPanel${(entranceAnimation) ? ' Enter' : ''}`}>
-        <div className={`ResumeContainer ${CTAEffect}`} onClick={downloadResume}>
-          <h1>Hey, my name's <span className="Accent">Josh</span>,</h1>
-          <h2 className="AlignChildrenVertical">
-            <span>and I'm a </span>
-            <span className={`WordCycler${(isAnimationComplete()) ? ' Settle' : ''}${(fastForward) ? ' FastForward' : ''}`}>
-              <span className="WordCyclePlane">{currentAdjective}</span>
-            </span> 
-            <span>developer.</span>
-          </h2>
+    <div className={`App${(hasLoaded) ? ' Loaded' : ''}`}>
+      <Router>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
+            <Route path="/">
+              <div className={`IntroPanel${(entranceAnimation) ? ' Enter' : ''}`}>
+                <div className={`CTAContainer ResumeContainer ${CTAEffect}`} onClick={downloadResume}>
+                  <h1>Hey, my name's <span className="Accent">Josh</span>,</h1>
+                  <h2 className="AlignChildrenVertical">
+                    <span>and I'm a </span>
+                    <span className={`WordCycler${(isAnimationComplete()) ? ' Settle' : ''}${(fastForward) ? ' FastForward' : ''}`}>
+                      <span className="WordCyclePlane">{currentAdjective}</span>
+                    </span> 
+                    <span>developer.</span>
+                  </h2>
+                  
+                  <button type="button" className="HomeCTA">R&eacute;sum&eacute;</button>
+                  
+                </div>
+                <Link className="CTAContainer PortfolioLinkContainer" to="/portfolio">
+                  <button type="button" className="HomeCTA SecondaryCTA">Portfolio</button>
+                </Link>
+              </div>
+              <hr className="BorderDecoration"/>
+            </Route>
+            <Route path="/portfolio">
+              <div className="WorkPanel">
+                <video autoPlay loop mute src="dawnofwar.mp4"></video>
+                <h1>I made <a href="https://www.dawnofwar.com">www.dawnofwar.com</a></h1>
+                <h2>I delivered numerous features on <a href="https://www.hardrocksocialcasino.com">www.hardrocksocialcasino.com</a></h2>
+                <h3>The same engine as <a href="https://play.star.com.au">play.star.com.au</a></h3>
+                <h3>and <a href="https://online.foxwoods.com">online.foxwoods.com</a></h3>
+                {/*<h2>I've built</h2>
+                <ul>
+                  <li>Features</li>
+                  <li>Tools</li>
+                  <li>Integrations</li>
+                  <li>Proofs of Concept</li>
+                </ul>*/}
+              </div>
+              <div className="SummaryPanel">
+                <h1>I have been <span className="Accent">building</span> software my whole life.</h1>
+                <h2>Love it when a <span className="Accent">plan</span> comes together.</h2>
+                <h2>Worked alongside <span className="Accent">brilliant</span> people.</h2>
+                <h2>Crave <span className="Accent">collaboration</span>.</h2>
+              </div>
+              <div className="ConnectPanel">
+                <h1>Let's chat! We have so much in common!</h1>
+                <ul className="SocialLinks">
+                  <li>
+                    <SocialIcon url="https://www.linkedin.com/in/agamedesigner/" />
+                  </li>
+                  <li>
+                    <SocialIcon url="https://www.facebook.com/josh.wood.12576" />
+                  </li>
+                  <li>
+                    <SocialIcon url="mailto:gdjoshwood@gmail.com" />
+                  </li>
+                  <li>
+                    <SocialIcon url="https://www.instagram.com/maremmazero/"/>
+                    </li>
+                </ul>
+              </div>
+            </Route>
+          </AnimatedSwitch>
+        </Router>
 
-          <button type="button" className="MainCTA">R&eacute;sum&eacute;</button>
-        </div>
-      </div>
-      <div className="SummaryPanel">
-        <h1>I have been <span className="Accent">building</span> software my whole life.</h1>
-        <h2>Love it when a <span className="Accent">plan</span> comes together.</h2>
-        <h2>Worked alongside <span className="Accent">brilliant</span> people.</h2>
-        <h2>Crave <span className="Accent">collaboration</span>.</h2>
-      </div>
-      <div className="WorkPanel">
-        <video autoPlay loop mute src="dawnofwar.mp4"></video>
-        <h1>I made <a href="https://www.dawnofwar.com">www.dawnofwar.com</a></h1>
-        <h2>I delivered numerous features on <a href="https://www.hardrocksocialcasino.com">www.hardrocksocialcasino.com</a></h2>
-        <h3>The same engine as <a href="https://play.star.com.au">play.star.com.au</a></h3>
-        <h3>and <a href="https://online.foxwoods.com">online.foxwoods.com</a></h3>
-        {/*<h2>I've built</h2>
-        <ul>
-          <li>Features</li>
-          <li>Tools</li>
-          <li>Integrations</li>
-          <li>Proofs of Concept</li>
-        </ul>*/}
-      </div>
-      <div className="DogPanel">
-        <h1>Also I love my dog.</h1>
-        <img className="Doggo" src="/zero.jpg"/>
-      </div>
-
-      <div className="ConnectPanel">
-        <h1>Let's chat! We have so much in common!</h1>
-        <ul className="SocialLinks">
-          <li>
-            <SocialIcon url="https://www.linkedin.com/in/agamedesigner/" />
-          </li>
-          <li>
-            <SocialIcon url="https://www.facebook.com/josh.wood.12576" />
-          </li>
-          <li>
-            <SocialIcon url="mailto:gdjoshwood@gmail.com" />
-          </li>
-          <li>
-            <SocialIcon url="https://www.instagram.com/maremmazero/"/>
-            </li>
-        </ul>
-      </div>
     </div>
   );
 }
