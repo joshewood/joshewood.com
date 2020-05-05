5dbf23e098fc35bd8f3d83f23bc1d8e400c6297d
Josh Wood <gdjoshwood@gmail.com>
2020-05-04 18:21:28 -0700

    🚀 finishing touches

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -67,7 +67,7 @@ function App() {
     }, 1000)
   }, []);  
   let location = useLocation();
-  return (<div className={`App${(hasLoaded) ? ' Loaded' : ''}`}>
+  return (<div className={`App${(hasLoaded) ? ' Loaded' : ''}${(location.pathname == '/') ? ' Home' : ''}`}>
       <TransitionGroup>
         {/*
           This is no different than other usage of
@@ -77,11 +77,12 @@ function App() {
         */}
         <CSSTransition
           key={location.key}
-          classNames="fade"
-          timeout={300}
+          classNames="slide"
+          timeout={900}
         >
           <Switch location={location}>
             <Route exact path="/">
+            <div>
               <div className={`IntroPanel${(entranceAnimation) ? ' Enter' : ''}`}>
                 <div className={`CTAContainer ResumeContainer ${CTAEffect}`} onClick={downloadResume}>
                   <h1>Hey, my name's <span className="Accent">Josh</span>,</h1>
@@ -89,50 +90,75 @@ function App() {
                   <button type="button" className="HomeCTA">R&eacute;sum&eacute;</button>
                   
                 </div>
+                <hr className="BorderDecoration"/>
                 <Link className="CTAContainer PortfolioLinkContainer" to="/portfolio">
                   <h2>I'm a developer</h2>
                   <button type="button" className="HomeCTA SecondaryCTA">Portfolio</button>
                 </Link>
+                <hr className="BorderDecoration"/>
               </div>
-              <hr className="BorderDecoration"/>
+              
+            </div>
             </Route>
             <Route path="/portfolio">
               <div className="WorkPanel">
-                {/*<video autoPlay loop mute src="dawnofwar.mp4"></video>*/}
-                <h1>I made <a href="https://www.dawnofwar.com">www.dawnofwar.com</a></h1>
-                <h2>I delivered numerous features on <a href="https://www.hardrocksocialcasino.com">www.hardrocksocialcasino.com</a></h2>
-                <h3>The same engine as <a href="https://play.star.com.au">play.star.com.au</a></h3>
-                <h3>and <a href="https://online.foxwoods.com">online.foxwoods.com</a></h3>
-                {/*<h2>I've built</h2>
-                <ul>
-                  <li>Features</li>
-                  <li>Tools</li>
-                  <li>Integrations</li>
-                  <li>Proofs of Concept</li>
-                </ul>*/}
-              </div>
-              <div className="SummaryPanel">
-                <h1>I have been <span className="Accent">building</span> software my whole life.</h1>
-                <h2>Love it when a <span className="Accent">plan</span> comes together.</h2>
-                <h2>Worked alongside <span className="Accent">brilliant</span> people.</h2>
-                <h2>Crave <span className="Accent">collaboration</span>.</h2>
-              </div>
-              <div className="ConnectPanel">
-                <h1>Let's chat! We have so much in common!</h1>
-                <ul className="SocialLinks">
-                  <li>
-                    <SocialIcon url="https://www.linkedin.com/in/agamedesigner/" />
-                  </li>
-                  <li>
-                    <SocialIcon url="https://www.facebook.com/josh.wood.12576" />
-                  </li>
-                  <li>
-                    <SocialIcon url="mailto:gdjoshwood@gmail.com" />
-                  </li>
-                  <li>
-                    <SocialIcon url="https://www.instagram.com/maremmazero/"/>
+                
+                <div className={`CTAContainer Short ResumeContainer ${CTAEffect}`} onClick={() => {window.location.assign('https://www.dawnofwar.com')}}>
+                  <a href="https://www.dawnofwar.com">www.dawnofwar.com</a>
+                  <video loop mute src="dawnofwar.mp4"></video>
+                </div>
+                
+                <div className={`CTAContainer ResumeContainer ${CTAEffect}`} onClick={() => {window.location.assign('https://www.hardrocksocialcasino.com')}}>
+                  <a href="#">Greentube Pro</a>
+                  <div className="GreentubeProTooltip">
+                    <ul className="GreentubeProClients">
+                      <li className="">
+                        <a href=">www.hardrocksocialcasino.com">
+                          <div>www.hardrocksocialcasino.com</div>
+                          <video loop mute src="hardrock.mp4"></video>
+                        </a>
+                      </li>
+                      <li className="">
+                        <a href=">online.foxwoods.com">
+                          <div>online.foxwoods.com</div>
+                          <video loop mute src="foxwoods.mp4"></video>
+                        </a>
+                      </li>
+                      <li className="">
+                        <a href=">play.star.com.au">
+                          <div>play.star.com.au</div>
+                          <video loop mute src="star.mp4"></video>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="VideoGallery">
+                    {/*<video loop mute src="dawnofwar.mp4"></video>
+                    <video loop mute src="dawnofwar.mp4"></video>
+                    <video loop mute src="dawnofwar.mp4"></video>*/}
+                  </div>
+                </div>
+                <div className={`CTAContainer ${CTAEffect}`} onClick={downloadResume}>
+                  <button type="button" className="HomeCTA NoMargin">R&eacute;sum&eacute;</button>
+                </div>
+
+                <div className={`CTAContainer SocialLinks`}>
+                  <h1>Let's chat! We have so much in common!</h1>
+                  <ul className="SocialLinks">
+                    <li>
+                      <SocialIcon url="https://www.linkedin.com/in/agamedesigner/" />
                     </li>
-                </ul>
+                    <li>
+                      <SocialIcon url="https://www.facebook.com/josh.wood.12576" />
+                    </li>
+                    <li>
+                      <SocialIcon url="mailto:gdjoshwood@gmail.com" />
+                    </li>
+                    <li>
+                      <SocialIcon url="https://github.com/joshewood/"/>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </Route>
           </Switch>
@@ -146,3 +172,27 @@ function App() {
 const BrowserApp = () => <Router><App/></Router>
 
 export default BrowserApp;
+
+              // <div className="SummaryPanel">
+              //   <h1>I have been <span className="Accent">building</span> software my whole life.</h1>
+              //   <h2>Love it when a <span className="Accent">plan</span> comes together.</h2>
+              //   <h2>Worked alongside <span className="Accent">brilliant</span> people.</h2>
+              //   <h2>Crave <span className="Accent">collaboration</span>.</h2>
+              // </div>
+              // <div className="ConnectPanel">
+              //   <h1>Let's chat! We have so much in common!</h1>
+              //   <ul className="SocialLinks">
+              //     <li>
+              //       <SocialIcon url="https://www.linkedin.com/in/agamedesigner/" />
+              //     </li>
+              //     <li>
+              //       <SocialIcon url="https://www.facebook.com/josh.wood.12576" />
+              //     </li>
+              //     <li>
+              //       <SocialIcon url="mailto:gdjoshwood@gmail.com" />
+              //     </li>
+              //     <li>
+              //       <SocialIcon url="https://www.instagram.com/maremmazero/"/>
+              //       </li>
+              //   </ul>
+              // </div>
