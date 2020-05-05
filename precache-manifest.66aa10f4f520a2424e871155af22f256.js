4c6a3dae209adf04b5d930ea91894e019cabb11f
Josh Wood <gdjoshwood@gmail.com>
2020-05-04 22:28:44 -0700

    Updates

---
+++ precache-manifest.66aa10f4f520a2424e871155af22f256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d1c2c85256a8926d6d8560d8677088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "419d81c4da24e0410a9d",
+    "url": "/static/css/main.76d456bc.chunk.css"
+  },
+  {
+    "revision": "a967b7a660390ea43701",
+    "url": "/static/js/2.511fbf08.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/2.511fbf08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419d81c4da24e0410a9d",
+    "url": "/static/js/main.6d09a98b.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
